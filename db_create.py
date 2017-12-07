8d5154cfe3dc6527c5aa17777d85ba827af681bf
JakubSokolowski <jakub.sokolowski1996@gmai.com>
2017-12-07 18:12:01 +0100

    Removed SQL module and db_create script

--- db_create.py
+++
@@ -1,9 +0,0 @@
-import database.ads_parser
-import database.ads
-
-#database.ads.create_ads_db('ads_2016_11_01.db')
-#database.ads.create_ads_db('ads_2016_12_01.db')
-#database.ads.create_ads_db('001_anonimized.db')
-#database.ads_parser.parse("/home/jakub/DataNinja/data/ads/001_anonimized", 10)
-database.ads_parser.parse2("/home/jakub/DataNinja/data/ads/001_anonimized", 1000)
-
