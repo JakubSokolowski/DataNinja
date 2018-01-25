eea5dc437bb11c389ff5ee089e5e1316fff7f36e
JakubSokolowski <jakub.sokolowski1996@gmai.com>
2018-01-25 18:50:26 +0100

    Remove the hardcoded path for data sources and add relative data path for the same sources

--- db/__init__.py
+++ db/__init__.py
@@ -1,7 +1,27 @@
+from pathlib import Path
 
-db_name =''
-db_path = '/home/jakub/DataNinja/data/database/'
+# Relative path to all the .db and .csv files in project
+# Assumed to by two folders up from the db module folder
+# ..../main_data_path/DataNinja/db
+# /main_data_path/ ----> data  ------> ads
+#                  |           ------> database
+#                  |           ------> search_queries
+#                  |
+#                  ----> DataNinja --> db
+#                                  --> docs
+#                                  --> notebooks
+#                                  --> sandbox
 
-ads_data_path = '/home/jakub/DataNinja/data/ads/#/001_anonimized'
-search_queries_data_path = '/home/jakub/DataNinja/data/search_queries/#'
-categories_path = '/home/jakub/DataNinja/data/categories.csv'
+main_data_path = str(Path(__file__).parents[2]) + "/data/"
+
+db_path = main_data_path + 'database/'
+ads_data_path = main_data_path + 'ads/#/001_anonimized'
+search_queries_data_path = main_data_path + 'search_queries/#'
+categories_path = main_data_path + 'categories.csv'
+
+# print(db_path)
+# print(ads_data_path)
+# print(search_queries_data_path)
+# print(categories_path)
+
+
