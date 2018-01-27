bf9ad3839008f06e905687b729aa1dabe74148c0
JakubSokolowski <jakub.sokolowski1996@gmai.com>
2018-01-27 22:17:22 +0100

    Add data module, for extracting data from database

---
+++ db/data/sampler.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import db.queries.string_quries as sq
+
+
+def sample_ads(conn, size, random=False):
+    """
+    Returns sample of ads table specified by connection
+    :param conn: Connection to sql database
+    :type conn: sqlite3.Connection
+    :param size: sample size, set to 'MAX' to return whole table
+    :param random: specifies whether the sample will be random
+    :return: the pandas.DataFrame with sample
+    """
+    query = sq.all_ads_limit.replace('#', str(size))
+    if size == 'MAX':
+        query = sq.all_ads
+    if random:
+        query = sq.all_ads_random.replace('#', str(size))
+    return pd.read_sql_query(query, conn)
+
+
+def sample_queries(conn, size, random=True):
+    """
+        Returns sample of queries table specified by connection
+        :param conn: Connection to sql database
+        :type conn: sqlite3.Connection
+        :param size: sample size, set to 'MAX' to return whole table
+        :param random: specifies whether the sample will be random
+        :return: the pandas.DataFrame with sample
+    """
+    query = sq.all_queries_limit.replace('#', str(size))
+    if size == 'MAX':
+        query = sq.all_queries
+    if random:
+        query = sq.all_ads_random.replace('#', str(size))
+    return pd.read_sql_query(query, conn)
