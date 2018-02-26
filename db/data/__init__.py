0f6689c2381c9efc58e7623d3001b83a349939cd
JakubSokolowski <jakub.sokolowski1996@gmai.com>
2018-02-26 17:41:19 +0100

    Add helper variables to init.py. Add parsing to csv to parser

---
+++ db/data/__init__.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+raw_data_path = str(Path(__file__).parents[3]) + "/months-data-raw/"
+parsed_data_path = str(Path(__file__).parents[3]) + "/months-data-parsed/"
+
+# 1  id integer primary key,
+# 2  region_id integer,
+# 3  category_id integer,
+# 4  subregion_id integer,
+# 5  district_id integer,
+# 6  city_id integer,
+# 7  accurate_location boolean,
+# 8  user_id,
+# 9  sorting_date datetime,
+# 10 created_at_first datetime,
+# 11 valid_to datetime,
+# 12 title text,
+# 13 description text,
+# 14 full_description text,
+# 15 has_phone boolean,
+# 16 params text,
+# 17 private_business boolean,
+# 18 has_person boolean,
+# 19 photo_sizes text,
+# 20 paidads_id_index integer,
+# 21 paidads_valid_to integer,
+# 22 predict_sold datetime,
+# 23 predict_replies integer,
+# 24 predict_views integer,
+# 25 reply_call integer,
+# 26 reply_sms integer,
+# 27 reply_chat integer,
+# 28 reply_call_intent integer,
+# 29 reply_chat_intent integer
+
+# List of not needed columns
+drop_list = [5, 7, 11, 13, 14, 18, 25, 26, 27, 28, 29]
+
+# Names of all the columns needed for processing
+col = ['id', 'region_id', 'category_id', 'subregion_id', 'city_id',
+       'user_id', 'sorting_date', 'created_at_first',
+       'title', 'has_phone', 'params', 'pricate_business',
+       'photo_sizes', 'paidads_id_index', 'paidads_valid_to',
+       'predict_sold', 'predict_replies', 'predict_views']
--- db/data/parser.py
+++ db/data/parser.py
@@ -1,13 +1,22 @@
 import _sqlite3
 import csv
 import os
+import os.path
 import re
+from os import listdir
+from os.path import isfile, join
+
+import pandas as pd
 
 import db.queries.insertions as q
 from db import ads_data_path
 from db import categories_path
 from db import db_path
 from db import search_queries_data_path
+from db.data import col
+from db.data import drop_list
+from db.data import parsed_data_path
+from db.data import raw_data_path
 
 
 def parse_categories(path):
@@ -112,63 +121,96 @@ def parse_ads(db_name):
     return
 
 
-# def parse_ads_to_df(db_name):
-#     """
-#        Parses ads from 001_anonimized file for given month and inserts them into
-#        database. File and database are specified by db_name
-#        The full path is created by joining the db.db_path with db_name
-#
-#        :param db_name: The name to the database
-#        :type db_name: str
-#
-#        """
-#
-#     db_name = ads_data_path.replace('#', db_name)
-#     with open(db_name) as infile:
-#         # Read whole file at once
-#         file_string = infile.read()
-#
-#         # Entry columns are separated by ","
-#         # Entries are separated by "\n"\
-#
-#         # Sometimes, there will be "","" used in product description, e.g "Movie Title 2"
-#         # do not split on such cases
-#         all_columns = re.split('(?<![0-9a-zA-Z?! ]\")\",\"(?!\"[0-9a-zA-Z])|(?<!\n)\"\n\"(?!\")', file_string)
-#
-#         entry = []
-#         column_counter = 0
-#         entry_counter = 0
-#
-#         for column in all_columns:
-#             # There are 29 columns in entry
-#             if column_counter == 29:
-#                 # Sometimes there are leftover " in first column
-#                 # get rid of them
-#                 entry[0] = entry[0].strip('"')
-#
-#                 # DEBUG - if there are any illegal chars in user product description
-#                 # it prints where they are so that we can fix/remove them
-#                 print(entry)
-#
-#                 # if is_date(entry[10]) and is_zero_or_one(entry[14]):
-#                 #    print("OK!")
-#
-#                 entries = map(replace_f_t, entry)
-#                 # Add the entry to the db
-#                 # WARNING - unfiltered tuple input, assuming that
-#                 # none of the OLX users is named DROP TABLE ADS
-#                 if entry_counter != 0:
-#                     q.ads_insert(cursor, tuple(entries))
-#
-#                 entry = []
-#                 column_counter = 0
-#
-#                 entry_counter += 1
-#
-#             entry.append(column)
-#             column_counter += 1
-#         conn.commit()
-#     return
+def drop_columns(entry, drop):
+    dropped_count = 1
+    for column in drop:
+        del entry[column - dropped_count]
+        dropped_count += 1
+    return entry
+
+
+def parse_ads_to_csv(filename):
+    """
+       Parses ads db_file and writes it to csv.
+       :param filename: The path file containing all the months data
+       :type filename: str
+    """
+
+    with open(raw_data_path + filename) as infile:
+        # Read whole file at once
+        file_string = infile.read()
+
+        # Entry columns are separated by ","
+        # Entries are separated by "\n"\
+
+        # Sometimes, there will be "","" used in product description, e.g "Movie Title 2"
+        # do not split on such cases
+        all_columns = re.split('(?<![0-9a-zA-Z?! ]\")\",\"(?!\"[0-9a-zA-Z])|(?<!\n)\"\n\"(?!\")', file_string)
+
+        # Create dict with row index as key and row as value
+        # The dict will be later passed to DataFrame
+
+        all_rows = dict.fromkeys(range(1, 1000000))
+        all_rows.update((k, []) for k in range(1, 1000000))
+        row = []
+        column_counter = 0
+        entry_counter = 0
+
+        for column in all_columns:
+            # There are 29 columns in entry
+            if column_counter == 29:
+                # Sometimes there are leftover " in first column
+                # get rid of them
+                row[0] = row[0].strip('"')
+
+                # DEBUG - if there are any illegal chars in user product description
+                # it prints where they are so that we can fix/remove them
+
+                # if is_date(entry[10]) and is_zero_or_one(entry[14]):
+                #    print("OK!")
+
+                row = map(replace_f_t, row)
+                row = drop_columns(list(row), drop_list)
+                if len(row) != 18:
+                    print("Row length error, actual len: ", len(row))
+                    return
+
+                if entry_counter != 0:
+                    all_rows[entry_counter] = row
+
+                row = []
+                column_counter = 0
+                entry_counter += 1
+
+            row.append(column)
+            column_counter += 1
+
+        df = pd.DataFrame.from_dict(all_rows, orient='index')
+        df.columns = col
+        # Sort the values by ad index
+        df['id'] = df['id'].apply(pd.to_numeric)
+        df = df.sort_values(['id'])
+        name = filename.split('/')[-1] + '.csv'
+        df.to_csv(parsed_data_path + name, sep=',', index=False)
+    return
+
+
+def write_all_ads_to_csv(adlist):
+    for ad in adlist:
+        if not os.path.isfile(raw_data_path + ad):
+            continue
+        parse_ads_to_csv(ad)
+    return
+
+
+def get_filenames(path):
+    """
+    Returs the list of all the filenames in directory specified by path
+    :param path: the path to directory
+    :return: filename list
+    """
+    return [f for f in listdir(path) if isfile(join(path, f))]
+
 
 def extract_date(path_str):
     """
@@ -245,6 +287,3 @@ def fill_db(db_name):
 
 def fill_all_dbs():
     return
-
-
-fill_db('2017_03.db')
